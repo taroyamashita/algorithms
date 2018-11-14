ef74e9bc75b8234d6828ce0512020964694822e7
Taro yamashita <taro.c.yamashita@gmail.com>
2018-11-14 11:13:28 -0500

    implement BFS

---
+++ TreeBFS2.js
@@ -0,0 +1,98 @@
+var Tree = function(value, depth = 0){
+    this.value = value;
+    this.children = [];
+    this.depth = depth;
+  };
+  
+  Tree.prototype.BFSelect = function(filter) {
+    var queue = new Queue();
+    let result = [];
+    queue.enqueue(this);
+      let node = queue.dequeue();
+      while (node) {
+        node.children.map(child => queue.enqueue(child));
+        result.push(node);
+        node = queue.dequeue();
+      }
+    
+    return result.filter(el => filter(el.value, el.depth)).map(el => el.value);
+  
+  };
+
+Tree.prototype.addChild = function(child){
+    if (!child || !(child instanceof Tree)){
+      child = new Tree(child, this.depth+1);
+    }
+  
+    if(!this.isDescendant(child)){
+      this.children.push(child);
+    }else {
+      throw new Error('That child is already a child of this tree');
+    }
+    // return the new child node for convenience
+    return child;
+  };
+  
+  /**
+    * check to see if the provided tree is already a child of this
+    * tree __or any of its sub trees__
+    */
+  Tree.prototype.isDescendant = function(child){
+    if(this.children.indexOf(child) !== -1){
+      // `child` is an immediate child of this tree
+      return true;
+    }else{
+      for(var i = 0; i < this.children.length; i++){
+        if(this.children[i].isDescendant(child)){
+          // `child` is descendant of this tree
+          return true;
+        }
+      }
+      return false;
+    }
+  };
+  
+  /**
+    * remove an immediate child
+    */
+  Tree.prototype.removeChild = function(child){
+    var index = this.children.indexOf(child);
+    if(index !== -1){
+      // remove the child
+      this.children.splice(index,1);
+    }else{
+      throw new Error('That node is not an immediate child of this tree');
+    }
+  };
+
+  var Queue = function() {
+    var storage = [];
+  
+    this.enqueue = function(item) {
+      storage.push(item);
+    };
+  
+    this.dequeue = function() {
+      return storage.shift();
+    };
+  };
+
+var root1 = new Tree(1); // depth 0
+var branch2 = root1.addChild(2); // depth 1
+var branch3 = root1.addChild(3); // depth 1
+var leaf4 = branch2.addChild(4); // depth 2
+var leaf5 = branch2.addChild(5); // depth 2
+var leaf6 = branch3.addChild(6); // depth 2
+var leaf7 = branch3.addChild(7); // depth 2
+
+root1.BFSelect(function (value, depth) {
+  return value % 2;
+}) //=> [1, 3, 5, 7]
+
+root1.BFSelect(function (value, depth) {
+  return depth === 1;
+}) //=> [2, 3]
+
+console.log(root1.BFSelect(function (value, depth) {
+    return value % 2;
+  })) //=> [1, 3, 5, 7]
--- alphabeticAnagrams.js
+++ alphabeticAnagrams.js
@@ -1,39 +1,53 @@
-function anagramPosition (string) {
+function anagramPosition (string){
     const sorted = string.split('').sort();
-    let count = 1;
+    console.log('sorted is', sorted);
+    let count = 1; 
     string.split('').forEach(char => {
-      const rank = sorted.indexOf(char);
-      const wasCounted = {}; 
-      sorted.slice(0, rank).forEach((el, i) => { 
-        if (!wasCounted[el]) {
-          count += countAnagrams(withoutIndex(i, sorted));
-        }
-        wasCounted[el] = true;
-      });
-      sorted.splice(rank, 1);
-    });
+        const rank = sorted.indexOf(char);
+        console.log('rank is', rank);
+        const wasCounted = {};
+        console.log(sorted.slice(0,rank));
+        sorted.slice(0, rank).forEach((el, i)=>{
+            console.log('element is', el);
+            if(!wasCounted[el]){
+                console.log(count);
+                console.log(withoutIndex(i, sorted));
+                console.log(countAnagrams(withoutIndex(i, sorted)));
+                count += countAnagrams(withoutIndex(i, sorted));
+            }
+            wasCounted[el] = true;
+        });
+        sorted.splice(rank,1);
+    })
     return count;
-  }
-  
-  // Formula: n!/(n1!n2!...nk!)
-  function countAnagrams (array) {
+}
+
+
+function countAnagrams (array){
     const nFactorial = factorial(array.length);
     const charOccurences = countChars(array);
-    const factorialsProduct = Object.keys(charOccurences).map(char => factorial(charOccurences[char])).reduce((a,b) => a * b);
-    return nFactorial / factorialsProduct;
-  }
-  
-  // Helper functions
-  const countChars = array => array.reduce((counter, char) => {counter[char] = counter[char] + 1 || 1; return counter}, {});
-  
-  const factorial = n => n === 1 ? 1 : n * factorial(n - 1);
-  
-  const withoutIndex = (i, array) => array.slice(0, i).concat(array.slice(i + 1));
+    const factorialsProduct = Object.keys(charOccurences).map(char=> factorial(charOccurences[char])).reduce((a,b)=> a*b);
+    return nFactorial / factorialsProduct; 
+}
+const factorial = n => n===1 ? 1 : n * factorial(n-1);
+const countChars = array => array.reduce((counter,char)=>{
+    counter[char] = counter[char] + 1 || 1; return counter
+},{});
+const withoutIndex = (i, array) => array.slice(0,i).concat(array.slice(i+1));
 
-  let test1 = "BAEBEE";
-  let test2 = "ABBS";
-  let test3 = "BABS";
-  let test4 =  "ARCTIC";
-  let test5 = "STARK"; 
+let testArray = ['a','a','b','d','g'];
 
-  console.log (anagramPosition(test5));
+let charOccurrences = countChars(testArray);
+
+let factorialsProduct = Object.keys(charOccurrences).map(char => factorial(charOccurrences[char])).reduce((a,b)=> a*b);
+
+console.log(withoutIndex(3, testArray));
+
+let test1 = "BAEBEE";
+let test2 = "ABBS";
+let test3 = "BABS";
+let test4 = "ARCTIC";
+let test5 = "STARK"; 
+
+console.log(anagramPosition(test5));
+
